35b3346c9897ac3ecb544b168ef3815ce768690c
ASattar04 <sattarahmed204@gmail.com>
2025-11-05 17:00:06 -0500

    working cli

--- backend/include/aka_goat.h
+++ backend/include/aka_goat.h
@@ -8,3 +8,7 @@
 typedef bool (*compare_func)(SoccerPlayer&, SoccerPlayer&, op);
 
 void run();
+std::string get_string_selection(int selection);
+void run_sorting(std::vector<SoccerPlayer> &tim_vec, std::vector<SoccerPlayer> &quick_vec, std::vector<SoccerPlayer> &merge_vec, int selection);
+compare_func get_compare_function(int selection);
+int print_options();
--- backend/src/aka_goat.cpp
+++ backend/src/aka_goat.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <windows.h>
 #include "tim.h"
 #include "quick.h"
 #include "soccerPlayer.h"
@@ -25,15 +26,10 @@ int print_options()
     {
         std::cout << osm::feat( osm::col, "yellow" ) << BREAK << osm::feat( osm::rst, "color" ) << ENDL;
         std::cout << "Pick an attirbute to sort by" << ENDL << ENDL;
-        std::cout << osm::feat( osm::col, "green" ) << "1. " << osm::feat( osm::rst, "color" ) << "Dominance Score" << ENDL;
-        std::cout << osm::feat( osm::col, "green" ) << "2. " << osm::feat( osm::rst, "color" ) << "Total Goals" << ENDL;
-        std::cout << osm::feat( osm::col, "green" ) << "3. " << osm::feat( osm::rst, "color" ) << "Total Assista" << ENDL;
-        std::cout << osm::feat( osm::col, "green" ) << "4. " << osm::feat( osm::rst, "color" ) << "Total Minutes Played" << ENDL;
-        std::cout << osm::feat( osm::col, "green" ) << "5. " << osm::feat( osm::rst, "color" ) << "Total Yellow Cards" << ENDL;
-        std::cout << osm::feat( osm::col, "green" ) << "6. " << osm::feat( osm::rst, "color" ) << "Total Red Cards" << ENDL;
-        std::cout << osm::feat( osm::col, "green" ) << "7. " << osm::feat( osm::rst, "color" ) << "Total Clean Sheets" << ENDL;
-        std::cout << osm::feat( osm::col, "green" ) << "8. " << osm::feat( osm::rst, "color" ) << "National Team Goals" << ENDL;
-        std::cout << osm::feat( osm::col, "green" ) << "9. " << osm::feat( osm::rst, "color" ) << "National Team Games Played" << ENDL;
+        for (int i = 1; i < 10; i++)
+        {
+            std::cout << osm::feat( osm::col, "green" ) << i << ". " << osm::feat( osm::rst, "color" ) << get_string_selection(i) << ENDL;
+        }
         std::cout << osm::feat( osm::col, "yellow" ) << BREAK << osm::feat( osm::rst, "color" ) << ENDL;
 
         std::cout << "Selection (1 - 9): ";
@@ -101,6 +97,149 @@ compare_func get_compare_function(int selection)
     }
 }
 
+void run_sorting(std::vector<SoccerPlayer> &tim_vec, std::vector<SoccerPlayer> &quick_vec, std::vector<SoccerPlayer> &merge_vec, int selection)
+{
+    std::cout << osm::feat( osm::col, "yellow" ) << BREAK << osm::feat( osm::rst, "color" ) << ENDL;
+    std::cout << "Sorting ..." << ENDL << ENDL;
+
+    // Tim sort
+    std::cout << "Running " << osm::feat( osm::col, "green" ) << "Tim Sort" << osm::feat( osm::rst, "color" ) << ENDL;
+
+    auto tim_start = std::chrono::high_resolution_clock::now();
+    timSort(tim_vec, get_compare_function(selection));
+    auto tim_end = std::chrono::high_resolution_clock::now();
+
+    // Quick sort
+    // std::cout << "Running " << osm::feat( osm::col, "green" ) << "Quick Sort" << osm::feat( osm::rst, "color" ) << ENDL;
+
+    // auto quick_start = std::chrono::high_resolution_clock::now();
+    // quickSort(quick_vec, 0, quick_vec.size() - 1, get_compare_function(selection), LESS_THAN);
+    // auto quick_end = std::chrono::high_resolution_clock::now();
+
+    // Merge sort
+    std::cout << "Running " << osm::feat( osm::col, "green" ) << "Merge Sort" << osm::feat( osm::rst, "color" ) << ENDL << ENDL;
+
+    auto merge_start = std::chrono::high_resolution_clock::now();
+    mergeSort(merge_vec, 0, merge_vec.size() - 1, get_compare_function(selection));
+    auto merge_end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> tim_elapsed = tim_end - tim_start;
+    // std::chrono::duration<double> quick_elapsed = quick_end - quick_start;
+    std::chrono::duration<double> merge_elapsed = merge_end - merge_start;
+
+    std::cout << "Elapsed time Tim Sort: " << osm::feat( osm::col, "green" ) << tim_elapsed.count() << osm::feat( osm::rst, "color" ) << " seconds\n";
+    // std::cout << "Elapsed time Quick Sort: " << osm::feat( osm::col, "green" ) << quick_elapsed.count() << osm::feat( osm::rst, "color" ) << " seconds\n";
+    std::cout << "Elapsed time Merge Sort: " << osm::feat( osm::col, "green" ) << merge_elapsed.count() << osm::feat( osm::rst, "color" ) << " seconds\n";
+
+    std::cout << osm::feat( osm::col, "yellow" ) << BREAK << osm::feat( osm::rst, "color" ) << ENDL;
+}
+
+std::string get_string_selection(int selection)
+{
+    switch (selection)
+    {
+    case 1:
+        return "Dominance Score";
+        break;
+
+    case 2:
+        return "Total Goals";
+        break;
+
+    case 3:
+        return "Total Assists";
+        break;
+
+    case 4:
+        return "Total Minutes Played";
+        break;
+
+    case 5:
+        return "Total Yellow Cards";
+        break;
+
+    case 6:
+        return "Total Red Cards";
+        break;
+
+    case 7:
+        return "Total Clean Sheets";
+        break;
+
+    case 8:
+        return "National Team Goals";
+        break;
+
+    case 9:
+        return "National Team Games Played";
+        break;
+    
+    default:
+        return "";
+        break;
+    }
+}
+
+int get_attribute(SoccerPlayer player, int selection)
+{
+    switch (selection)
+    {
+    case 1:
+        return (int)player.dominanceScore;
+        break;
+
+    case 2:
+        return player.totalGoals;
+        break;
+
+    case 3:
+        return player.totalAssists;
+        break;
+
+    case 4:
+        return player.totalMinPlayed;
+        break;
+
+    case 5:
+        return player.totalYellowCards;
+        break;
+
+    case 6:
+        return player.totalRedCards;
+        break;
+
+    case 7:
+        return player.totalCleanSheets;
+        break;
+
+    case 8:
+        return player.nationalTeamGoals;
+        break;
+
+    case 9:
+        return player.nationalTeamGames;
+        break;
+    
+    default:
+        return 0;
+        break;
+    }
+}
+
+std::string set_cursor_position(int row, int col)
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+
+    int termRows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    int termCols = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+
+    int targetRow = termRows - row;
+
+    return "\033[" + std::to_string(targetRow) + ";" + std::to_string(col) + "H";
+    
+}
+
 void run()
 {
     std::vector<SoccerPlayer> players_vector;
@@ -130,42 +269,56 @@ void run()
 
     // Display Sorting Time
 
-    // Tim sort
-    std::cout << "Running Tim Sort" << ENDL;
+    run_sorting(tim_sorted_list, quick_sorted_list, merge_sorted_list, selection);
 
-    auto tim_start = std::chrono::high_resolution_clock::now();
-    timSort(tim_sorted_list, get_compare_function(selection));
-    auto tim_end = std::chrono::high_resolution_clock::now();
+    // Display stats
 
-    // Quick sort
-    std::cout << "Running Quick Sort" << ENDL;
+    std::string string_selection = get_string_selection(selection);
 
-    auto quick_start = std::chrono::high_resolution_clock::now();
-    quickSort(quick_sorted_list, 0, quick_sorted_list.size() - 1, get_compare_function(selection), LESS_THAN);
-    auto quick_end = std::chrono::high_resolution_clock::now();
+    std::cout << ENDL;
 
-    // Merge sort
-    std::cout << "Running Merge Sort" << ENDL;
+    std::cout << set_cursor_position(1, 80);
+    std::cout << osm::feat( osm::col, "yellow" ) << "TOP 10" << osm::feat( osm::rst, "color" ) << ENDL << ENDL << ENDL;
 
-    auto merge_start = std::chrono::high_resolution_clock::now();
-    mergeSort(merge_sorted_list, 0, merge_sorted_list.size() - 1, get_compare_function(selection));
-    auto merge_end = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 10; i++)
+    {
+        SoccerPlayer player = tim_sorted_list.at(tim_sorted_list.size() - (i+1));
+        std::string padded_string;
+        int spaces = (30 - player.name.length());
+        if (spaces > 0)
+        {
+            padded_string = player.name + std::string(spaces, ' ');
+        }
+        std::cout << set_cursor_position(1, 80);
+        std::string num_string = std::to_string(i + 1) + ". ";
+        std::cout << std::left << std::setw(4) << num_string << osm::feat( osm::col, "yellow" ) << padded_string << osm::feat( osm::rst, "color" ) << ": " << string_selection << ": " << osm::feat( osm::col, "green" ) << get_attribute(player, selection) << osm::feat( osm::rst, "color" ) << ENDL << ENDL;
+    }
 
-    std::chrono::duration<double> tim_elapsed = tim_end - tim_start;
-    std::chrono::duration<double> quick_elapsed = quick_end - quick_start;
-    std::chrono::duration<double> merge_elapsed = merge_end - merge_start;
+    // Display GOAT
 
-    std::cout << "Elapsed time Tim Sort: " << osm::feat( osm::col, "green" ) << tim_elapsed.count() << osm::feat( osm::rst, "color" ) << " seconds\n";
-    std::cout << "Elapsed time Quick Sort: " << osm::feat( osm::col, "green" ) << quick_elapsed.count() << osm::feat( osm::rst, "color" ) << " seconds\n";
-    std::cout << "Elapsed time Merge Sort: " << osm::feat( osm::col, "green" ) << merge_elapsed.count() << osm::feat( osm::rst, "color" ) << " seconds\n";
+    std::cout << set_cursor_position(13, 0);
+    std::cout << osm::feat( osm::col, "green" ) << "GOAT: " << osm::feat( osm::rst, "color" );
 
-    std::cout << tim_sorted_list.at(tim_sorted_list.size() - 1).name << "; Dominance Score: " << tim_sorted_list.at(tim_sorted_list.size() - 1).dominanceScore << std::endl;
-    std::cout << tim_sorted_list.at(tim_sorted_list.size() - 2).name << "; Dominance Score: " << tim_sorted_list.at(tim_sorted_list.size() - 2).dominanceScore << std::endl;
-    std::cout << tim_sorted_list.at(tim_sorted_list.size() - 3).name << "; Dominance Score: " << tim_sorted_list.at(tim_sorted_list.size() - 3).dominanceScore << std::endl;
+    SoccerPlayer player = tim_sorted_list.at(tim_sorted_list.size() - 1);
+
+    std::cout << osm::feat( osm::col, "yellow" ) << player.name << osm::feat( osm::rst, "color" ) << ": " << string_selection << ": " << osm::feat( osm::col, "green" ) << get_attribute(player, selection) << osm::feat( osm::rst, "color" );
     
+    cv::Mat img = get_image_data(player.image, TIMEOUT);
+
+    std::cout << set_cursor_position(11, 0);
+
+    print_image_data(img);
+
+    std::cout << set_cursor_position(0, 0);
+
+    // while(1);
 
     // Display GOAT on the left, with name, stats, and a picture
 
     // Display other 9 on the right
+
+    // Ask to continue
+
+
 }
 
